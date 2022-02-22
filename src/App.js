ed626bb5df0a095ed6e33238d36028c4a0bb9767
yeynii <yeynii@gmail.com>
2022-02-22 20:39:12 +0900

    parallax 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="scene one">
+        <header>
+          <h1>Cartagena</h1>
+          색감이 아름다운 도시
+        </header>
+      </section>
+      <section className="scene two">
+        <header>
+          <h1>Medellin</h1>
+          붉은 벽돌집의 아룸다움이 있는 도시
+        </header>
+      </section>
+      <section className="scene three">
+        <header>
+          <h1>San gil</h1>
+          액티비티의 천국
+        </header>
+      </section>
     </div>
   );
 }
