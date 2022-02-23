a2384986688c79c70ad9c688865a9f7664e4265e
yeynii <yeynii@gmail.com>
2022-02-23 13:58:41 +0900

    경로 애니메이션 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import {useScrollY} from "./hooks/useScrollY";
+import {useState, useEffect} from "react";
 
 function App() {
+  const {scrollY} = useScrollY();
+  const [route, setRoute] = useState();
+
+  useEffect(() => {
+    scrollY && setRoute(scrollY / window.innerHeight)
+  }, [scrollY]);
+
   return (
       <div className="App">
         <section className="scene one">
@@ -21,25 +30,50 @@ function App() {
             액티비티의 천국
           </header>
         </section>
+        <section className="scene four">
+          <header>
+            <h1>Bogota</h1>
+            콜롬비아의 수도
+          </header>
+        </section>
         <div className="map">
           <img className="mapImg" src="col.png" alt="콜롬비아"/>
-          <label className="cartagena">
+          <div className="cartagena">
             <img className="pin" src="pin.png"/>
             <div className="diagonal">
-              <div className="fill"/>
+              <div className="fill"
+                   style={scrollY / window.innerHeight <= 0 ? {display: "none"} : route >= 1 ? {height: "100%"} : {height: route * 100 + "%"}}/>
             </div>
-          </label>
-          <label className="medellin">
+            <div className="placeName">
+              카르타헤나
+            </div>
+          </div>
+          <div className="medellin">
             <img className="pin" src="pin.png"/>
-            <div className="diagonal"/>
-          </label>
-          <label className="sangil">
+            <div className="diagonal">
+              <div className="fill"
+                   style={scrollY / window.innerHeight <= 1 ? {display: "none"} : route >= 2 ? {height: "100%"} : {height: (route - 1) * 100 + "%"}}/>
+            </div>
+            <div className="placeName">
+              메데진
+            </div>
+          </div>
+          <div className="sangil">
             <img className="pin" src="pin.png"/>
-            <div className="diagonal"/>
-          </label>
-          <label className="bogota">
+            <div className="diagonal">
+              <div className="fill"
+                   style={scrollY / window.innerHeight <= 2 ? {display: "none"} : {height: (route - 2) * 100 + "%"}}/>
+            </div>
+            <div className="placeName">
+              산힐
+            </div>
+          </div>
+          <div className="bogota">
             <img className="pin" src="pin.png"/>
-          </label>
+            <div className="placeName">
+              보고타
+            </div>
+          </div>
         </div>
       </div>
   );
---
+++ src/hooks/useScrollY.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export function useScrollY() {
+  const [scrollY, setScrollY] = useState(0);
+
+  const listener = e => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  });
+
+  return {
+    scrollY, setScrollY
+  };
+}
