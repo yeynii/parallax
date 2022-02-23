50e761e05fc5ff439a00b97656cbb679780f21f4
yeynii <yeynii@gmail.com>
2022-02-23 10:56:14 +0900

    대각선 추가

--- src/App.js
+++ src/App.js
@@ -2,41 +2,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <section className="scene one">
-        <header>
-          <h1>Cartagena</h1>
-          색감이 아름다운 도시
-        </header>
-      </section>
-      <section className="scene two">
-        <header>
-          <h1>Medellin</h1>
-          붉은 벽돌집의 아룸다움이 있는 도시
-        </header>
-      </section>
-      <section className="scene three">
-        <header>
-          <h1>San gil</h1>
-          액티비티의 천국
-        </header>
-      </section>
-      <div className="map">
-        <img className="mapImg" src="col.png" alt="콜롬비아"/>
-        <label classNames="cartagena">
-          <img className="pin" src="pin.png"/>
-        </label>
-        <label classNames="medellin">
-          <img className="pin" src="pin.png"/>
-        </label>
-        <label classNames="sangil">
-          <img className="pin" src="pin.png"/>
-        </label>
-        <label classNames="bogota">
-          <img className="pin" src="pin.png"/>
-        </label>
+      <div className="App">
+        <section className="scene one">
+          <header>
+            <h1>Cartagena</h1>
+            색감이 아름다운 도시
+          </header>
+        </section>
+        <section className="scene two">
+          <header>
+            <h1>Medellin</h1>
+            붉은 벽돌집의 아룸다움이 있는 도시
+          </header>
+        </section>
+        <section className="scene three">
+          <header>
+            <h1>San gil</h1>
+            액티비티의 천국
+          </header>
+        </section>
+        <div className="map">
+          <img className="mapImg" src="col.png" alt="콜롬비아"/>
+          <label className="cartagena">
+            <img className="pin" src="pin.png"/>
+            <div className="diagonal">
+              <div className="fill"/>
+            </div>
+          </label>
+          <label className="medellin">
+            <img className="pin" src="pin.png"/>
+            <div className="diagonal"/>
+          </label>
+          <label className="sangil">
+            <img className="pin" src="pin.png"/>
+            <div className="diagonal"/>
+          </label>
+          <label className="bogota">
+            <img className="pin" src="pin.png"/>
+          </label>
+        </div>
       </div>
-    </div>
   );
 }
 
